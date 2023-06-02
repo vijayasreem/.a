e475f67b3111adc55c5b71a60cd11acd210c0d2f
dotnet-code <@dotnet-code>
2023-06-02 14:12:58 +0530

    commit from code202306021303

---
+++ master/src/app/story-swami/story-swami.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story-swami',
+  templateUrl: './story-swami.component.html',
+  styleUrls: ['./story-swami.component.css']
+})
+export class StorySwamiComponent implements OnInit {
+
+  errorMessage: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.errorMessage = {
+      error: 'NoneType object has no attribute replace'
+    };
+  }
+
+}
